ea74858f9fb60acea9a5d9d54704b7fa9641f10c
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2023-10-08 19:13:20 -0400

    Made slider minimum and maximum values

--- Assets/Scripts/ScrollScript.cs
+++ Assets/Scripts/ScrollScript.cs
@@ -14,9 +14,11 @@ public class ScrollScript : MonoBehaviour
     public GameObject section; //Put only the Choose Outfit game object here
     public Scrollbar scroller;
     private float scrollval;
+    public float xMinimum;
+    public float xMaximum;
     
     public void OutfitsScroll() {
         scrollval = scroller.value;
-        section.transform.localPosition = new Vector2(Mathf.Lerp(-1000,0,scrollval),section.transform.localPosition.y);
+        section.transform.localPosition = new Vector2(Mathf.Lerp(xMinimum,xMaximum,scrollval),section.transform.localPosition.y);
     }
 }
