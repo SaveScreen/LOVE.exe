5c4413885efc1fe008e7784d25cf6f3ef78b4a29
Ethan_Branham <122489064+EtanX3@users.noreply.github.com>
2023-10-26 21:47:39 -0400

    Sliding game functionality exists

--- Assets/Scripts/DragScript.cs
+++ Assets/Scripts/DragScript.cs
@@ -7,7 +7,23 @@ public class DragScript : MonoBehaviour
 
     Vector2 difference = Vector2.zero;
 
+    //Collision stuff
+    public Camera cam;
+    public Transform DragObj;
+    public GameObject Canvas;
+    public float distanceFromCamera;
+    Rigidbody r;
+    [SerializeField] private float Speed;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        distanceFromCamera = Vector3.Distance(DragObj.position, cam.transform.position);
+        r = DragObj.GetComponent<Rigidbody>();
+        Canvas.SetActive(false);
+    }
+
+    /*
     private void OnMouseDown()
     {
         difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
@@ -16,22 +32,44 @@ public class DragScript : MonoBehaviour
     {
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
     }
+    */
 
+    Vector3 lastPos;
     private void Update()
     {
-        
+        Speed = (r.velocity.x * r.velocity.y);
+        if (Input.GetMouseButton (0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = distanceFromCamera;
+            pos = cam.ScreenToWorldPoint(pos);
+            r.velocity = (pos - DragObj.position) * 10;
+           // lastPos = pos;
+           // DragObj.position = pos;
+        }
+        if (Input.GetMouseButtonUp (0))
+        {
+            r.velocity = Vector3.zero;
+        }
+        /*
+        if (Speed >= 1000)
+        {
+            Speed = 500;
+        }
+        if (Speed <= -1000)
+        {
+            Speed = -500;
+        }
+        */
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "killbox")
+        if (other.gameObject.CompareTag("killbox"))
         {
             Debug.Log("Hit Player");
-        }
-        if (other.tag == "wall")
-        {
-            Debug.Log("cannot move farther");
-
+            gameObject.SetActive(false);
+            Canvas.SetActive(true);
         }
 
     }
