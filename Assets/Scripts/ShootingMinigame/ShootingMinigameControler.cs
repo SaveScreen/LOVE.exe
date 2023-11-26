fd2671a3f6a67456fd8e0c75f34bc896af271796
Ethan_Branham <122489064+EtanX3@users.noreply.github.com>
2023-11-26 18:13:15 -0500

    shooting minigame revamped

--- Assets/Scripts/ShootingMinigame/ShootingMinigameControler.cs
+++ Assets/Scripts/ShootingMinigame/ShootingMinigameControler.cs
@@ -2,52 +2,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ShootingMinigameControler : MonoBehaviour
 {
     Camera cam;
     public GameObject prefab;
     public TargetScript Script;
-    public TargetScript[] targets;
-    public List<TargetScript> myTargets = new();
+    public GameObject[] targetsArray;
+    public GameObject[] movingtargetsArray;
+
+    public GameObject playerdatacontainer;
+    private PlayerData playerdata;
+
+    public TextMeshProUGUI endlesscountdown;
+    public TextMeshProUGUI speedtext;
+    public TextMeshProUGUI AmmoCountText;
 
     public bool waitover;
 
-    public void ResetTargets()
-    {
-        if(waitover)
-        {
-            foreach(TargetScript target in myTargets)
-            {
-                target.Reactivate();
-                Debug.Log("all reactivated");
-                waitover = false;
-            }
-        }
-    }
+    [Header("Timer Stuff")]
+    public float timer; //Time in between circle spawning
+    private float starttimer;
+    private float respawntime;
+    private int timerinseconds;
+    [SerializeField] private float countdowntimer;
+
+    [Header("Ammo Stuff")]
+    public int currentClip, maxClipSize = 10;
+    private bool gamestarted;
+    [Header("Screens")]
+    public GameObject game;
+    public GameObject gameoverscreen;
+    public TextMeshProUGUI gameovertext;
+    public GameObject endlessgameoverscreen;
+    public GameObject endlessgamewinscreen;
+    public bool gameover;
+    public bool didWin;
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public GameObject MusicPlayer;
+    public AudioClip loseAudio;
+    public AudioClip winAudio;
+
+    [Header("Score")]
+    public int score;
+    public int requiredscore;
+    private int hiscore;
+    public TextMeshProUGUI hiscoretext;
+    public TextMeshProUGUI scoretext;
+    public TextMeshProUGUI requiredscoretext;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerdata = playerdatacontainer.GetComponent<PlayerData>();
+        hiscore = playerdata.GetRhythmGameHiScore();
+        scoretext.text = "Score: ";
+        requiredscoretext.text = "score at least " + requiredscore + " to win!";
+
         cam = Camera.main;
         print(cam.name);
         TargetScript target = gameObject.GetComponent<TargetScript>();
         waitover = false;
+
+        respawntime = timer;
+        starttimer = timer;
+        timerinseconds = 1;
+
+        gamestarted = false;
+        speedtext.text = "Circle/s: " + respawntime;
+
+        gameover = false;
+        gameoverscreen.SetActive(false);
+        endlessgameoverscreen.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            RandomTargetSelect();
+            timer = respawntime;
+            ShrinkRespawnTime();
+        }
+
+
         if(Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+        UpdateAmmoText();
+
+        scoretext.text = "Score: " + score;
+        if (score > hiscore)
+        {
+            hiscore = score;
+            hiscoretext.text = "Hiscore: " + hiscore;
+        }
+    }
+
+    void Fire()
+    {
+        if (currentClip > 0)
         {
             Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit Hit = new RaycastHit();
 
+            currentClip--;
             if (Physics.Raycast(cameraRay, out Hit))
             {
                 if (Hit.collider.CompareTag("ShootTarget"))
                 {
                     Debug.Log("hit target");
-                    
+
                     Hit.collider.gameObject.GetComponent<TargetScript>().AddScore();
                     //target.GetComponent<TargetScript>().AddScore();
                 }
@@ -57,28 +133,107 @@ public class ShootingMinigameControler : MonoBehaviour
                 }
             }
         }
-        
-        foreach (TargetScript obj in targets) {
-        if (targets.All(obj => obj.Iwashit)) // or .Any to test for ... "any"
+        if (currentClip == 0)
         {
-            //timer.StopTimer();
-               StartCoroutine(ResetAllCoroutine());
-                ResetTargets();
+            GameOver();
         }
-        }
-        
+    }
+    void UpdateAmmoText()
+    {
+        AmmoCountText.text = $"{currentClip} / {maxClipSize}";
+    }
+    void ShrinkRespawnTime()
+    {
+        respawntime = respawntime - respawntime * 0.035f;
+        speedtext.text = "Circle/s: " + respawntime;
+
+        Debug.Log(respawntime);
     }
 
-    public IEnumerator ResetAllCoroutine()
+    void RandomTargetSelect()
     {
-        while(!waitover)
-        {
-         Debug.Log("Started Coroutine at timestamp : " + Time.time);
-         yield return new WaitForSeconds(1);
-         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-                 waitover = true;
-                  yield return null;
+        int ItemIndex = (Random.Range(0, targetsArray.Length));
+        int howMany = (Random.Range(0, 100));
+        int ismovingtarget = (Random.Range(0, 100));
 
+        if (howMany >= 60)
+        {
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
         }
+        else if (howMany <= 20)
+        {
+            //want to make it so depending on the howMany value, different numbers of targets get activated at same time
+            //Need to make ItemIndex generate multiple number for this case to work
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+        }
+        else if (howMany > 20 && howMany < 60)
+        {
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+            targetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+        }
+        else if (howMany > 60 && ismovingtarget >= 50)
+        {
+            movingtargetsArray[ItemIndex].GetComponent<TargetScript>().InitializeTargets();
+        }
+        
+
+        ShrinkRespawnTime();
+    }
+
+    IEnumerator TimerText()
+    {
+        yield return new WaitForSeconds(0.75f);
+        endlesscountdown.enabled = false;
+        gamestarted = true;
+        StopCoroutine(TimerText());
+    }
+
+    public void MinigameComplete()
+    {
+        gameover = true;
+        didWin = true;
+        Time.timeScale = 0;
+        endlessgamewinscreen.SetActive(true);
+        //SHOULD ONLY SHOW A CONTINUE BUTTON
+    }
+
+    public void GameOver()
+    {
+        if (score >= requiredscore)
+        {
+            Debug.Log(didWin);
+            Time.timeScale = 0;
+            gameoverscreen.SetActive(true);
+            gameovertext.text = "You win";
+            MusicPlayer.SetActive(false);
+            PlaySound(winAudio);
+            gameover = true;
+            didWin = true;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            gameover = true;
+            gameoverscreen.SetActive(true);
+            gameovertext.text = "You lose";
+            MusicPlayer.SetActive(false);
+            PlaySound(loseAudio);
+            gameover = true;
+            didWin = false;
+        }
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Sharpshoot Minigame");
+    }
+
+
+    public void PlaySound(AudioClip audio)
+    {
+        audioSource.PlayOneShot(audio);
     }
 }
--- Assets/Scripts/ShootingMinigame/TargetScript.cs
+++ Assets/Scripts/ShootingMinigame/TargetScript.cs
@@ -4,55 +4,109 @@ using UnityEngine;
 
 public class TargetScript : MonoBehaviour
 {
+    private GameObject minigamemaster;
+    private ShootingMinigameControler SMC;
+
+    private Vector3 spawnpos;
+    public Vector3 uprightpos;
+    public Vector3 currentpos;
 
     [SerializeField] private Animator animator;
     public AudioClip snare;
     public AudioSource audiosource;
 
     //public Animation TargetSpin;
-    private Vector3 dir = Vector3.left;
-    private Vector3 negdir = Vector3.right;
+    private Vector3 dir = Vector3.forward;
+    private Vector3 negdir = -Vector3.forward;
     public float speed;
-    private bool movingLeft;
+    public float ismovingspeed;
+    private float velocity;
+    public bool movingLeft;
     public bool Iwashit;
+    [Header("types of targets")]
+    public bool movingtarget;
+    public bool ismoving;
+
+    public int scoreValue;
 
     // Start is called before the first frame update
     void Start()
     {
         movingLeft = true;
+        ismoving = false;
         Iwashit = false;
+        spawnpos = transform.position;
+        minigamemaster = GameObject.FindWithTag("Rhythmgame");
+        SMC = minigamemaster.GetComponent<ShootingMinigameControler>();
     }
 
     // Update is called once per frame
-
-
-    public void AddScore() {
-        audiosource.PlayOneShot(snare);
-        animator.SetTrigger("Tapped");
-        //TargetSpin.Play();
-        Debug.Log("AddScore");
-        //rms.score += 1;
-        transform.Translate(0,0,-1);
-        Iwashit = true;
-    }
-    public void Reactivate()
-    {
-        Iwashit = false;
-        transform.Translate(0,0,1);
-    }
-
     //Your Update function
 void Update(){
-     
 
-        if (movingLeft == true) {
-        // move left
-        transform.Translate(dir*speed*Time.deltaTime);
-        if (transform.position.x >= 4) movingLeft = false;
-        } else {
-        // move right
-        transform.Translate(negdir*speed*Time.deltaTime);
-        if (transform.position.x <= -4) movingLeft = true;
+        currentpos = transform.position;
+        if(movingtarget && ismoving)
+        {
+            if (movingLeft == true)
+            {
+                // move left
+                transform.Translate(dir * ismovingspeed * Time.deltaTime);
+                if (transform.position.x >= 5) movingLeft = false;
+            }
+            else
+            {
+                // move right
+                transform.Translate(negdir * ismovingspeed * Time.deltaTime);
+                if (transform.position.x <= -5) movingLeft = true;
+            }
         }
-}
+
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            InitializeTargets();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ResetTargets();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            flipuptargets();
+        }
+    }
+
+    public void AddScore()
+    {
+        audiosource.PlayOneShot(snare);
+        //animator.SetTrigger("Tapped");
+        //TargetSpin.Play();
+        Debug.Log("AddScore");
+        SMC.score += scoreValue;
+        transform.Translate(0, 0, -1);
+        Iwashit = true;
+        ismoving = false;
+        transform.position = spawnpos;
+    }
+    public void ResetTargets()
+    {
+        Iwashit = false;
+        ismoving = false;
+        transform.position = spawnpos;
+    }
+
+    public void InitializeTargets()
+    {
+        Debug.Log("targets initialized");
+        if (!movingtarget)
+        {
+            transform.position = uprightpos;
+        }
+        
+        ismoving = !ismoving;
+    }
+    public void flipuptargets()
+    {
+        transform.position = uprightpos;
+    }
 }
